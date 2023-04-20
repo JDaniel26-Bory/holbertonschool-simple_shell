4d4eff44b4d1b084accb2e3233a1c58de832bd5d
Holberton <holberton@Holbertons-iMac.local>
2023-04-20 13:44:12 -0500

    fix the code and change the name of a file

---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int simple_shell(void);
+
+#endif
--- simple_shell.c
+++ simple_shell.c
@@ -1,52 +1,40 @@
-#include "main.h"
-/**
- * main - is the function principal
- * @void: is a variable
- *
- * Return: 0
- */
-int main(void)
+#include "shell.h"
+#include <ctype.h>
+
+#define BUFFER_SIZE 1024
+
+int simple_shell(void)
 {
-char *buffer = NULL;
-size_t buffer_size = 0;
-ssize_t chars_read;
-pid_t pid;
-int status;
-while (1)
-{
-printf("$ ");
-chars_read = getline(&buffer, &buffer_size, stdin);
-if (chars_read == -1)
-{
-if (errno == EINVAL)
-{
-perror("getline");
-}
-if (errno == ENOMEM)
-{
-perror("getline");
-}
-break;
-}
-buffer[strcspn(buffer, "\n")] = '\0';
-pid = fork();
-if (pid == -1)
-{
-perror("fork");
-exit(EXIT_FAILURE);
-}
-else if (pid == 0)
-{
-char *args[] = { buffer, NULL };
-execve(buffer, args, NULL);
-perror(buffer);
-exit(EXIT_FAILURE);
-}
-else
-{
-waitpid(pid, &status, 0);
-}
-}
-free(buffer);
-exit(EXIT_SUCCESS);
+    char *buffer = NULL;
+    size_t buffer_size = 0;
+    ssize_t chars_read;
+    pid_t pid;
+    int status;
+
+    while (1) {
+        printf("$ ");
+        chars_read = getline(&buffer, &buffer_size, stdin);
+        if (chars_read == -1) {
+            if (feof(stdin)) {
+                printf("\n");
+            }
+            break;
+        }
+        buffer[strcspn(buffer, "\n")] = '\0';
+
+        pid = fork();
+        if (pid == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        } else if (pid == 0) {
+            execlp(buffer, buffer, (char *)NULL);
+            perror(buffer);
+            exit(EXIT_FAILURE);
+        } else {
+            waitpid(pid, &status, 0);
+        }
+    }
+
+    free(buffer);
+    exit(EXIT_SUCCESS);
 }
