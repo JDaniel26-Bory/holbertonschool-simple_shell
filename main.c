c15759215feaaaad114f40700e4a0adc32b63196
Bluebee45 <Santi72143@gmail.com>
2023-04-21 15:14:23 -0500

    fix code

--- main.c
+++ main.c
@@ -1,40 +1,47 @@
 #include "shell.h"
-#define BUFFER_SIZE 1024
+
 
 int main(void)
 {
     char *buffer = NULL;
     size_t buffer_size = 0;
-    ssize_t chars_read;
-	  pid_t pid;
-				int status;
+    ssize_t c;
+    pid_t pid;
+    int status;
 
-    while (1) {
+    while (1)
+    {
         printf("$ ");
-        chars_read = getline(&buffer, &buffer_size, stdin);
-        if (chars_read == -1) {
-            if (feof(stdin)) {
-                printf("\n");
-            }
+        c = getline(&buffer, &buffer_size, stdin);
+        if (c == -1)
+        {
+            if (feof(stdin))
+                putchar('\n');
             break;
         }
         buffer[strcspn(buffer, "\n")] = '\0';
 
-        if (strlen(buffer) == 0 || strlen(buffer) > 20 || (((buffer[0] >= 'A') && (buffer[0] <= 'Z')) && ((buffer[0] >= 'a') && (buffer[0] <= 'z'))))
-						{
-            printf("Error: Código inválido\n");
-            continue;
-        }
-
         pid = fork();
-        if (pid == -1) {
+        if (pid == -1)
+        {
             perror("fork");
             exit(EXIT_FAILURE);
-        } else if (pid == 0) {
-            execlp(buffer, buffer, (char *)NULL);
-            perror(buffer);
+        }
+        else if (pid == 0)
+        {
+            char **args = split_line(buffer);
+            if (args == NULL)
+            {
+                fprintf(stderr, "Failed to tokenize command\n");
+                exit(EXIT_FAILURE);
+            }
+
+            execvp(args[0], args);
+            perror(args[0]);
             exit(EXIT_FAILURE);
-        } else {
+        }
+        else
+        {
             waitpid(pid, &status, 0);
         }
     }
